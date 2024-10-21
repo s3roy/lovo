299e31e090c2f1effab5e1685ee0fe87adca3a79
Souvik Roy <souvikroy1999ab@gmail.com>
2024-10-21 16:50:42 +0530

    added api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { AppProps } from "next/app";
+import { CSSReset, ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+function App({ Component, pageProps }: AppProps): React.ReactNode {
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+export default App;
